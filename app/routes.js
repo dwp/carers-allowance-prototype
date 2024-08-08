a7a74ecd36abd6b7dc20c90b686c6a3b0db03541
Colin Jones <colin.jones@engineering.digital.dwp.gov.uk>
2024-08-08 15:49:28 +0100

    Added 35 hours journey prototype

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,12 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/35-hours-past-date', function(request, response) {
+
+    var hoursOfCare = request.session.data['35HoursOfCare']
+    if (hoursOfCare == "yes"){
+        response.redirect("/layouts/apply/short-journeys/35-hours-yes")
+    } else {
+        response.redirect("/layouts/apply/short-journeys/35-hours-deflection")
+    }
+})
